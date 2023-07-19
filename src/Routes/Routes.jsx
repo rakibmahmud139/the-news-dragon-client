5ce8ee0279e4beae8aa09a8e167989c13653a813
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-19 22:34:44 +0600

    added vercel link

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category />,
-                loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({ params }) => fetch(`https://the-news-dragon-server-rakibmahmud139.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News /></PrivateRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({ params }) => fetch(`https://the-news-dragon-server-rakibmahmud139.vercel.app/news/${params.id}`)
             }
         ]
     }
